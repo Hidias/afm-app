e3d5f0b658a74d04a1948dcaa73e94239b031666
Hidias <hicham.saidi@accessformation.pro>
2026-01-23 21:36:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // Active le mode sombre avec la classe 'dark'
   theme: {
     extend: {
       colors: {
